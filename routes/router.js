82345f5834c6c5e839df51b7d7c026b96b8155d3
PoojanBhalodiya <poojanbhalodiya02@gmail.com>
2023-10-13 20:28:11 +0530

    setup basic getfunctionality

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
+const { getTodos } = require("../controllers/todo");
 
 router.get("/", (req, res) => {
   res.send("Let's build a CRUD API!");
 });
+
+//getTodos
+router.get("/todos", getTodos);
 
 module.exports = router;
